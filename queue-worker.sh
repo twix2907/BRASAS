67b077b90506ee97c4683ffa10dd0813ab1e10be
Jhanpool <twix2907@gmail.com>
2025-07-06 17:35:34 -0500

    Elimina archivos y limpia estructura

--- queue-worker.sh
+++
@@ -1,5 +0,0 @@
-# Azure WebJob for Laravel Queue Worker
-# This file helps Azure run the queue worker as a background process
-
-# Queue worker command
-cd /home/site/wwwroot && php artisan queue:work --verbose --tries=3 --timeout=90
